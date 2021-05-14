12f20f535fc093c8cb3ec9fff49c09249ab21767
nome do usuario <nome do email>
2021-05-14 13:43:31 -0300

    Corrigindo erro no wsgi.py

--- SGEI/wsgi.py
+++ SGEI/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+
 from dj_static import Cling, MediaCling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SGEI.settings')
 
 application = Cling(MediaCling(get_wsgi_application()))
+
