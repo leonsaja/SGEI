e96650626bf862ee99aab052e44cd15c30c84d73
leonsaja <leonardosaja87@gmail.com>
2021-05-30 21:51:15 -0300

    corrigindo erro

--- SGEI/settings.py
+++ SGEI/settings.py
@@ -21,7 +21,6 @@ DATABASES = {
 }
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i%5va=!n*kr&+t94x)obm^=sn_@wf9nu0p_(e2@l2a)o=9wuyt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
@@ -101,14 +100,14 @@ WSGI_APPLICATION = 'SGEI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
